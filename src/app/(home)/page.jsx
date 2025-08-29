dcd3de73069e87a9ab8b0b13615088bf06d5068d
sajid-islam <sajidislam941@gmail.com>
2025-08-29 07:05:44 +0600

    feat(ui): add banner slider components

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,12 +1,18 @@
+import BannerSlider from "@/components/BannerSlider/BannerSlider";
 import Navbar from "@/components/Navbar/Navbar";
 import PrimaryNavbar from "@/components/PrimaryNavbar/PrimaryNavbar";
+import SearchBar from "@/components/SearchBar/SearchBar";
 import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="space-y-10">
+    <div className="space-y-5 md:space-y-10">
       <Navbar />
       <PrimaryNavbar />
+      <div className="md:hidden px-5 relative">
+        <SearchBar />
+      </div>
+      <BannerSlider />
     </div>
   );
 };
---
+++ src/components/BannerSlider/BannerSlider.jsx
@@ -0,0 +1,37 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import Image from "next/image";
+
+import sliderImage1 from "@/images/sliderImage/Electronics.jpg";
+import sliderImage2 from "@/images/sliderImage/Beauty.jpg";
+import sliderImage3 from "@/images/sliderImage/Fashion.jpg";
+
+import { Pagination, Autoplay, Navigation } from "swiper/modules";
+
+const BannerSlider = () => {
+  return (
+    <div className="max-w-[90rem] px-5 mx-auto">
+      <Swiper
+        pagination={true}
+        navigation={true}
+        autoplay={{ delay: 2500, disableOnInteraction: false }}
+        modules={[Pagination, Autoplay, Navigation]}
+      >
+        <SwiperSlide>
+          <Image src={sliderImage1} alt="slider image 1" className="rounded-md" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={sliderImage2} alt="slider image 2" className="rounded-md" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={sliderImage3} alt="slider image 2" className="rounded-md w-full" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default BannerSlider;
